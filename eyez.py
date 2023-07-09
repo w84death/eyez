ba9ac53691d3022bf8ede544588cc856b03aca2a
Krzysztof Krystian Jankowski <kj@p1x.in>
2023-07-09 16:31:18 +0200

    adds birds detection (as example)

--- eyez.py
+++ eyez.py
@@ -5,7 +5,8 @@ import subprocess
 import threading
 from queue import Queue
 import time
-from collections import OrderedDict
+from datetime import datetime
+from collections import OrderedDict, Counter
 
 class Eyez:
     def __init__(self, frame_rate=4, timeout=3):
@@ -43,7 +44,9 @@ class Eyez:
                     output_image = draw_bbox(frame, bbox, label, conf)
 
                     now = time.time()
-                    for item in label:
+                    counts = Counter(label)
+
+                    for item in counts:
                         self.labels[item] = now
 
                     labels_to_remove = [k for k, v in self.labels.items() if now - v > self.timeout]
@@ -52,18 +55,24 @@ class Eyez:
                         if l in self.spoken_labels:
                             del self.spoken_labels[l]
 
-                    for item in label:
+                    for item, count in counts.items():
                         if item not in self.spoken_labels:
                             self.spoken_labels[item] = now
-                            self.audio_queue.put(item)
+                            item_with_count = f"{count} {item}" if count > 1 else item
+                            self.audio_queue.put(item_with_count)
+
+                            if item == 'bird':
+                                timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+                                filename = f"{timestamp}.jpg"
+                                cv2.imwrite(filename, frame)
 
                     labels_sorted = OrderedDict(sorted(self.labels.items(), key=lambda t: t[1]))
-                    text = ', '.join(labels_sorted.keys())
+                    text = ', '.join(f"{counts[item]} {item}" if counts[item] > 1 else item for item in labels_sorted)
 
                     if text != '':
                         cv2.putText(output_image, "I see a " + text + ".", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                     else:
-                         cv2.putText(output_image, "I see a nothing...", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                        cv2.putText(output_image, "I see nothing...", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                     cv2.imshow("EYEZ", output_image)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
